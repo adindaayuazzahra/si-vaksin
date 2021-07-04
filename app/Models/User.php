b2dbfcda21575b20d20f3c2e7e374aed303ed1c1
unknown <stevenchristpa2001@gmail.com>
2021-07-04 18:55:38 +0700

    pembeneran invoice

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
     //     'email_verified_at' => 'datetime',
     // ];
     public function informasiuser(){
-        return $this->hasOne(informasiUser::class, 'id_user');
+        return $this->hasOne(InformasiUser::class, 'id_user');
     }
 
     public function registrasi(){
